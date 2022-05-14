5d0431bc00705481f4b43e13c781ae9502815bf9
kirill.adamuk <kirill.adamuk@acronis.com>
2022-05-14 17:52:34 +0300

    test

---
+++ test/test_main.py
@@ -0,0 +1,9 @@
+import os
+from datetime import datetime
+
+
+def test_logger():
+    from app.logger import get_logger
+    log = get_logger('test_log')
+    log.info('test')
+    assert os.path.exists(f'../log/{datetime.now().strftime("%Y%m%d")}-test_log.log')
